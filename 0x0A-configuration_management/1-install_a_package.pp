2f67330ef8b33cd19ac5ee1651c750d22d5172ed
Adebayo Samuel <eladebayoor@gmail.com>
2022-09-17 03:14:41 +0100

    [fix]: simplified code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,7 @@
 # install flask from pip3
 
-$package = 'flask'
-
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update'
-}
-
-package { 'python3-pip':
-  ensure  => 'installed'
-  require => Exec['apt-get update']
-}
-
-exec { 'install flask':
-  command => '/usr/bin/pip3 install flask==2.1.0'
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
 
