4667786f34435f8d5ea47d628dfa614c6cfcc32a
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-11 05:55:33 +0100

    [fix]: code rewrite

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,6 @@
 # Fix 24 roo many requests
 
-exec {'fix limit':
-  command  => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
-  before   => Exec['restart'],
-}
-
-exec {'restart nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
